0ffdca9d671f788a0bb87dcce019cd6c82322289
ituitisk-ak19 <bilalak1907@gmail.com>
2025-01-05 23:34:25 +0300

    add not null annotations

--- src/main/java/com/madlen/madlen/model/ModelAnswer.java
+++ src/main/java/com/madlen/madlen/model/ModelAnswer.java
@@ -2,15 +2,19 @@ package com.madlen.madlen.model;
 
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.util.List;
 
 @Data
 @Embeddable
 public class ModelAnswer {
+    @NotNull
     private String mainArgument;
+    @NotNull
     private List<String> keyPoints;
     @ElementCollection
     private List<SupportingEvidence> supportingEvidence;
+    @NotNull
     private String conclusion;
 }
--- src/main/java/com/madlen/madlen/model/SupportingEvidence.java
+++ src/main/java/com/madlen/madlen/model/SupportingEvidence.java
@@ -1,11 +1,14 @@
 package com.madlen.madlen.model;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @Embeddable
 public class SupportingEvidence {
+    @NotNull
     private String point;
+    @NotNull
     private Integer pageReference;
 }
